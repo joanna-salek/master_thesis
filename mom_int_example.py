3b768e4cea3ffb1255b92150f3a3b088c70dd4e8
joanna-salek <joanna.salek.15@gmail.com>
2021-11-22 21:05:02 +0100

    moments of inertia

--- mom_int_example.py
+++ mom_int_example.py
@@ -1,4 +1,5 @@
-from chaos_game import Hurst_CGR, hurst_from_fasta, draw_tree, save_hurst_table
+from chaos_game import DNA, intertia_from_fasta, draw_tree
+import numpy as np
 
 ################################################
 # EXAMPLE 1
@@ -16,20 +17,17 @@ def example1():
             "AGAAATCTTCTCTGAGAGAGAGAGAAATCTTCTCTGAGAGAGAGAGAAATCT" \
             "TCTCTGAGAGAGAGAGAAATCTTCTCTGAGAG"
 
-      h = Hurst_CGR(seq)
-      print(h.get_hurst())
-      h.plot_CGR()
+      i = DNA(seq, 2, False)
+      print (i.DNA_descriptors())
 
 ################################################
 # EXAMPLE 2
 ################################################
 # read file from fasta with only one seq in it
-# you can enable drawing chaos game representation
-# and choose CGR type
 def example2():
-      hurst = hurst_from_fasta("one_file_test.fasta", True, CGR_types="RY")
-      for key in hurst.keys():
-            print(key, hurst[key])
+      intertia = intertia_from_fasta("one_file_test.fasta")
+      for key in intertia.keys():
+            print(key, intertia[key])
 
 ################################################
 # EXAMPLE 3
@@ -37,43 +35,24 @@ def example2():
 # read file from fasta with multiple seq in it
 # fasta parser will divide it on sequences and species
 def example3():
-      hurst = hurst_from_fasta("SPARC_refseq_transcript.fasta")
-      for key in hurst.keys():
-            print(key, hurst[key])
+    intertia = intertia_from_fasta("SPARC_refseq_transcript.fasta")
+    for key in intertia.keys():
+        print(key, intertia[key])
 
-################################################
-# EXAMPLE 4
-################################################
-# save as tables from pandas to hmtl file
-def example4():
-      hurst = hurst_from_fasta("SPARC_refseq_transcript.fasta")
-      save_hurst_table(hurst)
-
-################################################
-# EXAMPLE 5
-################################################
-# return dictionary
-def example5():
-      hurst = hurst_from_fasta("SPARC_refseq_transcript.fasta")
-      for key in hurst.keys():
-            i = hurst[key]
-            new_dict = {k: v for k, v in sorted(i.items(), key=lambda item: item[1])}
-      return new_dict
 
 ################################################
 # EXAMPLE 5
 ################################################
 # draw phylogenetic tree based on hurst values for species
 def example6():
-      hurst = hurst_from_fasta("SPARC_refseq_transcript.fasta")
+      intertia = intertia_from_fasta("SPARC_refseq_transcript.fasta")
       arr = []
-      for key in hurst["MK"].keys():
-            arr.append([hurst["MK"][key]])
-      draw_tree(arr, list(hurst["MK"].keys()), "SPARC gene phylogenetic tree - moments of inertia")
+      for key in intertia.keys():
+            arr.append(intertia[key])
+      draw_tree(arr, list(intertia.keys()), "SPARC gene phylogenetic tree - hurst method")
 
 # example1()
 # example2()
 # example3()
 # example4()
-# print (example5())
-# example6()
+example6()
